1672f267114d7b3345a0264c5608356d90709e6f
frannoi12 <francoistoyi4@gmail.com>
2023-07-04 17:01:36 +0000

    voici la total

--- regex/regexp/regexp.js
+++ regex/regexp/regexp.js
@@ -49,5 +49,115 @@ let masque2 = /[oeiau\^]/i
 
 document.getElementById("p1").innerHTML = chaine.match(masque1);
 document.getElementById("p2").innerHTML = chaine.match(masque2);
-*/
 
+
+let chaine = "Bonjour, je m'appelle Pierre et vous ?"
+
+let masque1 = /[aeiouy]/g;
+let masque2 = /j[aeiouy]/g;
+let masque3 = /[aeiouy][aeiouy]/g;
+let masque4 = /je/g;
+let masque5 = /j[aeiouy][aeiouy]/;
+let masque6 = /[et]/;
+let masque7 = /[ê]/g;
+document.getElementById("p1").innerHTML = chaine.match(masque7);*/
+
+// let chaine = " Bonjour, Je suis willy . Mon /numéro/ est le [70-02-26-00] 545444242 ";
+// let chaine1 = "1032123352454545458";
+
+// //1
+
+// let masque = /[^aeiouy]/gi;
+// let masque1 = /[aeiouy^]/gi;
+// let masque2 = /[a-zA-Z]o/g;
+// let masque3 = /[az-]/gi;
+// let masque4 = /[a-z0-9-]/g;
+// let masque5 = /[0-9\[\]]/g;
+// let masque6 = /[\da-z]/g;
+// let masque8 = /\d/g;
+// let masque7 = /[^\w]/g;
+
+// let masque01 = /\W/g;
+// let masque0 = /^[A-Z].{10,}/g;
+// let masque12 = /[\^|\$]/g;
+// let masque10 = /[o|j]/g;
+// let masque11 = /[Fadilou|Wiwayo]/g;
+// let masque13 = /\d{10,10}$/g;
+
+
+// document.getElementById("p1").innerHTML = chaine1.match(masque13);
+
+// 5.1
+
+//1
+
+// Récupérer la chaîne de texte à partir du h1
+let texteH1 = document.querySelector("h5").innerText;
+
+let masque0 = /^\s+|\s+$/g;
+// Supprimer les espaces blancs de début et de fin en utilisant une regex
+// let texteNettoye = texteH1.replace('');
+
+// document.getElementById("p1").innerHTML = texteH1.replace(masque0," ");
+
+
+//2
+
+// let masque1 = / {2,}/g;
+
+// document.getElementById("p1").innerHTML = texteH1.replace(masque1," ");
+
+
+//3
+
+// let chaine1 = "Une chaine \n pas comme \n les autres .";
+
+// let masque2 = /^\s*/gm;
+
+// document.getElementById("p1").innerHTML = chaine1.replace(masque2," ");
+
+// 5.2
+
+// 1
+
+// function verifierAdresseEmail(adresseEmail) {
+//     let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//     return regex.test(adresseEmail);
+//   }
+  
+
+
+// let adresseEmail = "kjbfe@domaine.com";
+// let estValide = verifierAdresseEmail(adresseEmail);
+
+// if (estValide) {
+//     console.log("Adresse e-mail valide");
+// } else {
+//     console.log("Adresse e-mail invalide");
+// }
+  
+
+// let chaineEssaie = "Bonjour voici le nouveau er texte d'essai et bcdes et abc.";
+
+
+// let masque1 = /er|t/g;
+// let masque2 = /e(r|t)/g;
+// let masque3 = /(ab)*c/g;
+// let masque4 = /(ab)|(bc)d/g;
+// const text = "<a><b><a><c><b><c>";
+// let masque = /(<.>).*\1/g;
+
+// document.getElementById("p1").innerHTML = text.match(masque);
+
+
+
+let chaine = "Bonjour, je suis Pierre et mon nom. est le [06-36-65-65-65]";
+
+
+// let masque1 = /e(?=r)/g;
+// let masque2 = /e(?!r)/g;
+// let masque3 = /(?<=i)s/g;
+// let masque4 = /(?<!i)s/g;
+
+
+document.getElementById("p1").innerHTML= chaine.match(masque4);
